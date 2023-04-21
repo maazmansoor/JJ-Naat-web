e2117563b992853861ed508eda59a93cdfee8216
maazmansoor <114831495+maazmansoor@users.noreply.github.com>
2023-04-21 22:10:23 +0500

    Add files via upload

---
+++ sound web/js/music-list.js
@@ -0,0 +1,86 @@
+let allMusic = [
+
+    {
+        name: "meetha-meetha-piyara",
+        artist: "Junaid Jamshed",
+        img: "download",
+        src: "meetha-meetha-piyara-piyara"
+    },
+    {
+        name: "kamli-wale",
+        artist: "Junaid Jamshed",
+        img: "download (3)",
+        src: "kamli-wale"
+    },
+    {
+        name: "mera-dil-badal-de",
+        artist: "Junaid Jamshed",
+        img: "mera-dil-badal-de",
+        src: "mera-dil-badal-de"
+    },
+    {
+        name: "muhammad-ka-roza",
+        artist: "Junaid Jamshed",
+        img: "download (2)",
+        src: "muhammad-ka-roza"
+    },
+    {
+        name: "har-shaks-ki-garden",
+        artist: "Junaid Jamshed",
+        img: "download",
+        src: "har-shaks-ki-garden"
+    },
+    {
+        name: "mohabbat-kya-hai",
+        artist: "Junaid Jamshed",
+        img: "OIP 4",
+        src: "mohabbat-kya-hai"
+    },
+    {
+        name: "na-koi-aap-sa-hoga",
+        artist: "Junaid Jamshed", 
+        img: "OIP 46",
+        src: "na-koi-aap-sa-hoga"
+    },
+    {
+        name: "qaseeda-burda-shareef",
+        artist: "Junaid Jamshed", 
+        img: "OIP 48",
+        src: "qaseeda-burda-shareef"
+    },
+    {
+        name: "tala-al-badru-alaina",
+        artist: "Junaid Jamshed", 
+        img: "OIP 47",
+        src: "tala-al-badru-alaina"
+    },
+    {
+        name: "ya-habibi-ya-rasooli",
+        artist: "Junaid Jamshed", 
+        img: "OIP3",
+        src: "ya-habibi-ya-rasooli-english"
+    },
+    {
+        name: "o-merciful",
+        artist: "Junaid Jamshed", 
+        img: "pexels-seej-nguyen-755416",
+        src: "o-merciful"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
